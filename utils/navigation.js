0feb74495821f38e75de8dedf009d1716f4eb025
Steven <steven.chang@pakingtek.com>
2019-04-11 19:04:55 +0800

    build global N sensor data and floor N sensor data

--- utils/navigation.js
+++ utils/navigation.js
@@ -3,12 +3,16 @@ var Graph = require('graph/graph.js')
 var Dijkstra = require('graph/dijkstraAlgorithm.js')
 var SensorDataModel = require('model/sensorDataModel.js')
 var svg;
-var sdm;
 var sensorData;
 var navQueue;
+var floorQueue;
 var callbackCount;
+var graphList = [];
+var navHashMap = [];
+var navSeqHashMap = [];
+var nowFloor = '4F';
 const FIRST_N_QSIZE = 30;
- 
+
 
 export class IndoorFindSpace {
   /**
@@ -20,38 +24,43 @@ export class IndoorFindSpace {
   constructor() {
     this.init();
     this.initMapGraph();
-  } 
+  }
   init() {
     navQueue = new Array();
-    callbackCount = 0; 
+    callbackCount = 0;
     console.log('====初始化室內導航====');
   }
-  initMapGraph(){  
+  initMapGraph() {
     SensorDataModel.sensorInfo.init();
     sensorData = SensorDataModel.sensorInfo.Data;
     if (sensorData !== undefined) {
       console.log('取得場內資訊');
       console.log(sensorData);
       this.buildMapGraph();
-    }else{
+      this.buildNavSeqHashMap()
+    } else {
       var pointer = this;
-      setTimeout(function () {
+      setTimeout(function() {
         pointer.initMapGraph();
       }, 2000);
     }
   }
   buildMapGraph() {
-    var graphList = [];
-    var isUpdateMap = false;
     for (var i = 0; sensorData[i]; i++) {
       if (!graphList[sensorData[i].floor]) {
         graphList[sensorData[i].floor] = new Graph.Graph();
       }
-      if (graphList[sensorData[i].floor]){
+      if (graphList[sensorData[i].floor]) {
         graphList[sensorData[i].floor].addNode(sensorData[i].seq_id);
-        graphList[sensorData[i].floor].addEdge(sensorData[i].seq_id, sensorData[i].Next_N,1);
+        var next = sensorData[i].Next_N.split(',')
+        for (var j = 0; next[j]; j++)
+          graphList[sensorData[i].floor].addEdge(sensorData[i].seq_id, next[j], 1);
       }
+      var info={[sensorData[i].sensor_id]:sensorData[i]};
+      navHashMap = Object.assign({}, navHashMap, info);
     }
+    console.log('建立全域N字典');
+    console.log(navHashMap);
     console.log('建立有向圖');
     console.log(graphList);
     // let dij = new Dijkstra.DijkstraAlgorithm(map)
@@ -59,10 +68,26 @@ export class IndoorFindSpace {
     // console.log(map.getEdge())
     // dij.changeCost(1, 3, 3);
   }
+  buildNavSeqHashMap(){
+    for (var i = 0; sensorData[i]; i++) {
+      var info = { [sensorData[i].sensor_id]: sensorData[i] };
+      if (nowFloor == sensorData[i].floor) {
+        var info = { [sensorData[i].seq_id]: sensorData[i] };
+        navSeqHashMap = Object.assign({}, navSeqHashMap, info);
+      }
+    }
+    console.log('建立當前樓層N字典');
+    console.log(navSeqHashMap);
+
+  }
+
+
   startIndoorNavigation(ble) {
     this.addBle2Queue(navQueue, ble);
 
   }
+
+  
   addBle2Queue(queue, ble) {
     var name = ble[0].name;
     var rssi = ble[0].RSSI;
@@ -77,4 +102,3 @@ export class IndoorFindSpace {
     }
   }
 }
-
